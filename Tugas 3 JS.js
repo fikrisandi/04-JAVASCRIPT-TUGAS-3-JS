c4081632d9a84c189a7bdc2c759b9509f391fba9
Muhammad Fikri Sandi Pratama <55092974+fikrisandi@users.noreply.github.com>
2021-02-14 18:55:34 +0700

    Tugas 3 JS

---
+++ Tugas 3 JS.js
@@ -0,0 +1,18 @@
+function nilai(x) {
+    console.log(x);
+    return x * 2;
+}
+
+const nilai2 = function(x) {
+    console.log(x);
+    return x * 3;
+}
+
+const nilai3 = (x) => {
+    console.log(x);
+    return x * 5;
+}
+
+console.log(nilai(7));
+console.log(nilai2(11));
+console.log(nilai3(13));
